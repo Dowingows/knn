73bb0866f69e5dc3e4014fbb156f4eb1be5a013d
Domingos Dias <domingos.adj@gmail.com>
2018-07-05 18:24:29 -0300

    * Modificações

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import csv
 import math
 import random
 import operator
+import copy
 
 '''
-K-NN utilizando o Iris dataset, apenas um teste para avaliar conhecimentos nesse algoritmo de classificação
+K-NN utilizando o dataset de atributos de jogadores para identificar se possuem características para ser atacantes ou defensores
 '''
 
-
+'''
+#====== INÍCIO DE FUNÇÕES PARA CALCULAR DISTÂNCIAS ENTRE VETORES
+'''
 def minkowski_distance(vector1, vector2, p):
     if len(vector1) != len(vector2):
         return -1
@@ -27,6 +30,9 @@ def manhattan_distance(vector1, vector2):
     return minkowski_distance(vector1, vector2, 1)
 
 
+'''
+#====== FIM DAS FUNÇÕES PARA CALCULAR DISTÂNCIAS ENTRE VETORES
+'''
 def load_dataset(filename, quant_numbers):
     dataset = []
 
@@ -43,7 +49,7 @@ def load_dataset(filename, quant_numbers):
     return dataset
 
 
-def load_train_and_test_dataset(filename, split):
+def load_train_and_test_dataset(filename, quant_numbers, split):
     training_set = []
     test_set = []
 
@@ -51,7 +57,7 @@ def load_train_and_test_dataset(filename, split):
         lines = csv.reader(csv_ds_file)
         dataset = list(lines)
         for x in range(len(dataset)):
-            for y in range(5):
+            for y in range(quant_numbers):
                 dataset[x][y] = float(dataset[x][y])
             if random.random() < split:
                 training_set.append(dataset[x])
@@ -60,6 +66,20 @@ def load_train_and_test_dataset(filename, split):
 
     return training_set, test_set
 
+
+def normalize_dataset(dataset, quant_numbers):
+    max_values = [0 for i in range(quant_numbers)]
+    min_values = [0 for i in range(quant_numbers)]
+
+    for i in range(quant_numbers):
+        max_values[i] = max(data[i] for data in dataset)
+        min_values[i] = min(data[i] for data in dataset)
+
+    for i in range(len(dataset)):
+        for j in range(quant_numbers):
+            dataset[i][j] = (dataset[i][j] - min_values[j])/(max_values[j]-min_values[j])
+
+    return dataset
 
 def normalize_attributes(element_1):
     new_element = element_1
@@ -73,9 +93,7 @@ def normalize_attributes(element_1):
     return new_element
 
 def get_similarity(element_1, element_2):
-    elm1 = normalize_attributes(element_1[:-1])
-    elm2 = normalize_attributes(element_2[:-1])
-    return euclidean_distance(elm1, elm2)
+    return euclidean_distance(element_1[:-1], element_2[:-1])
 
 
 def get_neighbors(training_set, test_element, k):
@@ -112,7 +130,9 @@ def get_accuracy(test_set, predictions):
 
 def training_test():
     # prepare data
-    training, test = load_train_and_test_dataset('jogadores-data.csv', 0.7)
+    training, test = load_train_and_test_dataset('jogadores-data.csv',5, 0.7)
+    training = normalize_dataset(training, 5)
+    test = normalize_dataset(test, 5)
     print("+------------+")
     print("Valores de treino e teste")
     print(training)
@@ -135,10 +155,8 @@ def training_test():
 
 
 def main():
-    dataset = load_dataset('jogadores-data.csv', 5)
-    classifications = load_dataset('instancias_para_classificar.csv', 5)
-    print('Dataset: ' + repr(len(dataset)))
-    print('Classification set: ' + repr(len(classifications)))
+    dataset = normalize_dataset(load_dataset('jogadores-data.csv', 5),5)
+    classifications = normalize_dataset(load_dataset('instancias_para_classificar.csv', 5),5)
 
     # generate predictions
     predictions = []
@@ -149,11 +167,18 @@ def main():
         result = get_response(neighbors)
         predictions.append((classifications[x][:-1], result))
 
-    with open('vale-nada.txt','a') as f:
-        print(predictions,file = f)
+    print_on_file_list(predictions,'test01.csv')
 
     print(predictions)
 
 
+def print_on_file_list(custom_list,file_name):
+    with open(file_name, 'a') as f:
+        print(custom_list, file = f)
+
+
+#training_test()
 main()
+
+
 
